77ab5eb4afbae9bec202ed20221b245eb1611442
Yasin Çalışkan <yasincaliskan@Yasins-MacBook-Pro.local>
2020-12-19 16:54:05 +0300

    Question & Answer components added with basic SCSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,11 @@
 import React from 'react';
+import Question from './components/survey/question/Question';
+import './css/main.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Question />
     </div>
   );
 }
