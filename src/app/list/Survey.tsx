c6a55710651420d41c5c8d5158a82d206863c253
Yasin Çalışkan <yasincaliskan@Yasins-MacBook-Pro.local>
2020-12-20 18:58:00 +0300

    Added QuestionList component

--- src/app/list/Survey.tsx
+++ src/app/list/Survey.tsx
@@ -1,9 +1,104 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+
+interface Question {
+  id: number,
+  content: string,
+  choices: Choice[]
+}
+
+interface Choice {
+  id: number,
+  content: string,
+  type?: string,
+  rate?: number
+}
 
 function Survey() {
+  const questionList: Question[] = [
+    {
+      id: 1,
+      content: "is it a question?",
+      choices: [
+        {
+          id: 1,
+          content: "choice A",
+          rate: 10
+        },
+        {
+          id: 2,
+          content: "choice B",
+          rate: 5
+        },
+        {
+          id: 3,
+          content: "choice C"
+        }
+      ]
+    },
+    {
+      id: 2,
+      content: "is it a question?",
+      choices: [
+        {
+          id: 1,
+          content: "choice A",
+          rate: 10
+        },
+        {
+          id: 2,
+          content: "choice B",
+          rate: 5
+        },
+        {
+          id: 3,
+          content: "choice C"
+        }
+      ]
+    },
+    {
+      id: 3,
+      content: "is it a question?",
+      choices: [
+        {
+          id: 1,
+          content: "choice A",
+          rate: 10
+        },
+        {
+          id: 2,
+          content: "choice B",
+          rate: 5
+        },
+        {
+          id: 3,
+          content: "choice C"
+        }
+      ]
+    }
+  ]
+
+  const [questions, setQuestions] = useState(questionList);
+
   return (
     <div>
-      survey page
+      <ol>
+        {questions.map((question) => (
+          <li>
+            {question.content}
+            <ul>
+              {question.choices.map((choice) => (
+                <li>
+                  <button>
+                  {choice.content}
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </li>
+        ))}
+      </ol>
+      <button>Send</button>
     </div>
   )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import Survey from './app/list/Survey';
 import ExamGeneratorView from './components/survey/Survey';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ExamGeneratorView />
+    <Survey />
   </React.StrictMode>,
   document.getElementById('root')
 );
