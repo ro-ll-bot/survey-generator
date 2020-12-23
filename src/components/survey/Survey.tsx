43e3882d6f362a04f7e4104e43da961bc218418f
bilginyuksel <bilgin.yuksel96@gmail.com>
2020-12-23 23:06:54 +0300

    choice component completely transformed to function component structure

--- src/components/survey/Survey.tsx
+++ src/components/survey/Survey.tsx
@@ -4,7 +4,6 @@ import { QuestionView, Question } from '../question/Question';
 export default class ExamGeneratorView extends React.Component {
 
   questionViews: JSX.Element[] = [];
-  questions: Question[] = [];
   questionViewRefs: React.RefObject<QuestionView>[] = [];
   state = {
     questionViews: this.questionViews
@@ -12,21 +11,22 @@ export default class ExamGeneratorView extends React.Component {
 
   addNewQuestion = () => {
     this.questionViewRefs.push(React.createRef<QuestionView>());
-    this.questionViews.push(<QuestionView
-      ref={this.questionViewRefs[this.questionViewRefs.length -1]} 
-      id={this.questionViewRefs.length}></QuestionView>);
+    this.questionViews.push(
+      <QuestionView
+        ref={this.questionViewRefs[this.questionViewRefs.length - 1]}
+        id={this.questionViewRefs.length} />);
     this.setState({ questions: this.questionViews });
   }
 
   createExam = () => {
-    
+    const questions: Question[] = [];
     this.questionViewRefs.forEach(ref => {
       const question: Question | undefined = ref.current?.getQuestion();
-      if(question !== undefined)
-        this.questions.push(question);
+      if (question !== undefined)
+        questions.push(question);
     });
 
-    console.log(JSON.stringify(this.questions));
+    console.log(JSON.stringify(questions));
   }
 
   render() {
