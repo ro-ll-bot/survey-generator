c32549b4806457d36a39e7a609d89681ee98be99
Yasin Çalışkan <yasincaliskandev@gmail.com>
2021-01-03 17:25:02 +0300

    Update TimeLine.tsx

--- src/app/timeline/TimeLine.tsx
+++ src/app/timeline/TimeLine.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { Survey, SurveyTimeLine } from '../../components/survey/Survey'
+import { Survey, SurveyTimeLine } from '../components/survey/Survey'
 
 interface TimeLine {
   surveyList: Survey[];
 }
 
-const mockSurveys = require("../../mockSurveyData.json");
+const mockSurveys = require("../mockSurveyData.json");
 
 export function TimeLine() {
   const surveyList: Survey[] = mockSurveys;
