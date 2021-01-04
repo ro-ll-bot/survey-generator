7a685a6d55752b35cf61e588863d795fc6610bc8
Hasancan Kazan <Hasancan.Kazan@logo.com.tr>
2021-01-04 23:37:47 +0300

    Edited navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,23 +1,34 @@
-import { useState } from "react";
+interface NavbarItemData {
+    url: string;
+    description: string;
+    isActive: boolean;
+}
+
+function NavbarItem(item: NavbarItemData) {
+    if (item.isActive)
+        return <a className="active" href={item.url}>{item.description}</a>
+    else
+        return <a href={item.url}>{item.description}</a>
+}
 
 export function Navbar() {
 
-    const navbarItems=
+    const navbarItems =
         [{
             "url": "#SurveyList",
             "description": "Survey List",
             "isActive": true
-        }, 
+        },
         {
             "url": "#CreateSurvey",
             "description": "Create Survey",
             "isActive": false
-        }, 
+        },
         {
             "url": "#MySurveys",
             "description": "My Surveys",
             "isActive": false
-        }, 
+        },
         {
             "url": "#Profile",
             "description": "Profile",
@@ -25,14 +36,11 @@ export function Navbar() {
         }];
 
     return (
-            <div className="nav">
-                <a href="#" className="logo" >Survey Genarator</a>
-                {navbarItems.map((item)=>{
-                    if(item.isActive)
-                        return <a className="active" href={item.url}>{item.description}</a>
-                    else
-                        return <a href={item.url}>{item.description}</a>
-                })}
-            </div>
+        <div className="nav">
+            <a href="#" className="logo" >Survey Genarator</a>
+            {navbarItems.map((item) => {
+                return NavbarItem(item);
+            })}
+        </div>
     )
 }
